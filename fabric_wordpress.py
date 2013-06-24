02b46e6ec7d7bba106d983037a9275b66469f006
Ernesto <erdaseq@gmail.com>
2013-06-23 23:23:38 -0300

    Path wordpress

--- fabric_wordpress.py
+++ fabric_wordpress.py
@@ -2,9 +2,12 @@
 from fabric.api import (
     env,
     sudo,
+    cd,
 )
 from fabric.colors import (
+    yellow,
     green,
+    red,
 )
 
 env.hosts = ['users@localhost:22']
@@ -13,18 +16,21 @@ env.password = 'password'
 name_old = '_old'
 name_new = '_new'
 
+
 def download_last_version():
-    print('>>> Update Wordpress...')
-    sudo('cd /var/www/wordpress')
+    print yellow('>>> Update Wordpress...')
+    path = raw_input("Enter path: ")
+    with cd('%s' % path)
     sudo('wget http://wordpress.org/latest.tar.gz')
     sudo('tar xzfv latest.tar.gz')
     sudo('rm latest.tar.gz')  
-    print('>>> done...')
+    print green('>>> done...')
     clean_download()
 
+
 def clean_download():
-    print('>>> Clean download...')
-    sudo('cd wordpress')
+    print yellow('>>> Clean download...')
+    with cd('wordpress')
     sudo('mv wp-admin wp-admin%s' % name_new)
     sudo('mv wp-includes wp-includes%s' % name_new)
     sudo('mv wp-admin%s wp-includes%s ..' %(name_new, name_new))  
@@ -32,22 +38,21 @@ def clean_download():
     print('>>> done...')
     backup_old_folders()
 
+
 def backup_old_folders():
-    print('>>> Backup...')
+    print yellow('>>> Backup...')
     sudo('mv wp-admin wp-admin%s' % name_old)
     sudo('mv wp-includes wp-includes%s' % name_old)
     sudo('mkdir backup')
-    sudo('mv wp-admin% wp-includes% backup' %(name_old, name_old))
+    sudo('mv wp-admin%s wp-includes%s backup' %(name_old, name_old))
     sudo('mv wp-admin%s wp-admin' % name_new)
     sudo('mv wp-includes%s wp-include' % name_new)
-    print('>>> done...')
-    print('>>> Visited http://yourdomain.com/wp-admin/upgrade.php to finish update...')
+    print green('>>> done...')
+    print red('>>> Visited http://yourdomain.com/wp-admin/upgrade.php to finish update...')
+
 
 def deploy():
     download_last_version()
-
-
-
 
 
 
